6fc9748fc06d23a49a5aa57408f34997065010f6
rakshit <rakshit.modi.d@gmail.com>
2025-03-09 22:02:58 +0530

    solving errors

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 
 app = Flask(__name__)
 
 @app.route('/first', methods=['GET'])
 def first():
-    response = jsonify({"message": "Success"})
-    response.status_code = 200
+    response = make_response(jsonify({"message": "Success"}), 200)
     response.headers['Content-Type'] = 'application/json'
-    response.headers['Authorization'] = 'Bearer token123'
+    response.headers['Authorization'] = 'Bearer token123'  # Consider removing hardcoded token
     return response
 
 @app.route('/second', methods=['GET'])
 def second():
-    response = jsonify({"param1": "value1", "param2": "value2"})
-    response.status_code = 400
+    # If this is an error response, return an error message
+    response = make_response(jsonify({
+        "error": "Invalid parameters",
+        "param1": "value1",
+        "param2": "value2"
+    }), 400)
+    
     response.headers['Content-Type'] = 'application/json'
-    response.headers['Authorization'] = 'Bearer token123'
+    response.headers['Authorization'] = 'Bearer token123'  # Consider removing hardcoded token
     return response
+
+# Global Error Handler
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({"error": "Bad request"}), 400)
 
 if __name__ == '__main__':
     app.run(debug=True)
